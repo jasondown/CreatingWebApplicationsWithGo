02242ffb0f1dcf8efa64e0b6e16467b67e01ebce
Jason Down <jdown@igloosoftware.com>
2018-04-06 23:54:50 -0400

    Began custom file system.

---
+++ src/src/github.com/lss/webapp/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		f, err := os.Open("public" + r.URL.Path)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Println(err)
+			defer f.Close()
+			io.Copy(w, f)
+		}
+	})
+}
