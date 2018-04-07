82a913e43d839b212ad3a3290f0ebe07c5f0e664
Jason Down <jdown@igloosoftware.com>
2018-04-07 00:28:34 -0400

    Fixed up some path issues.

--- src/github.com/lss/webapp/main.go
+++ src/github.com/lss/webapp/main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		f, err := os.Open("CreatingWebApplicationsWithGo/src/public" + r.URL.Path)
+		f, err := os.Open("src/creatingwebapplicationswithgo/public" + r.URL.Path)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			log.Println(err)
