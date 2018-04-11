0bf4cf082bc4cdca4862c13ab393f8fd70b04c3c
Jason Down <jdown@igloosoftware.com>
2018-04-11 00:42:44 -0400

    PopulateTemplates now creates family of templates by cloning layout and then grabbing and parsing unique content templates.

--- src/github.com/lss/webapp/main.go
+++ src/github.com/lss/webapp/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -25,9 +27,35 @@ func main() {
 	http.ListenAndServe(":8000", nil)
 }
 
-func populateTemplates() *template.Template {
-	result := template.New("templates")
+func populateTemplates() map[string]*template.Template {
+	result := make(map[string]*template.Template)
 	const basePath = "src/CreatingWebApplicationsWithGo/templates"
-	template.Must(result.ParseGlob(basePath + "/*.html"))
+	layout := template.Must(template.ParseFiles(basePath + "/_layout.html"))
+	template.Must(layout.ParseFiles(basePath+"/_header.html", basePath+"/_footer.html"))
+	dir, err := os.Open(basePath + "/content")
+	if err != nil {
+		panic("Failed to open template blocks directory: " + err.Error())
+	}
+	fis, err := dir.Readdir(-1)
+	if err != nil {
+		panic("Failed to read contents of content directory: " + err.Error())
+	}
+	for _, fi := range fis {
+		f, err := os.Open(basePath + "/content" + fi.Name())
+		if err != nil {
+			panic("Failed to open template '" + fi.Name() + "'")
+		}
+		content, err := ioutil.ReadAll(f)
+		if err != nil {
+			panic("Failed to read content from file '" + fi.Name() + "'")
+		}
+		f.Close()
+		tmpl := template.Must(layout.Clone())
+		_, err = tmpl.Parse(string(content))
+		if err != nil {
+			panic("Failed to parse contents of '" + fi.Name() + "' as template")
+		}
+		result[fi.Name()] = tmpl
+	}
 	return result
 }
