15d1eae9ddd319daade852fe244b1a2deec3178c
Jason Down <jdown@igloosoftware.com>
2018-04-12 22:21:10 -0400

    Added custom function via FuncMap and accessed it in the template.

--- src/examplePipelines/main.go
+++ src/examplePipelines/main.go
@@ -12,16 +12,12 @@ type Product struct {
 	Price float32
 }
 
-func (p Product) PriceWithTax() float32 {
-	return p.Price * (1 + tax)
-}
-
 const templateString = `
 {{- "Item Information:" }}
 -----------------
 Name: {{ .Name }}
 Price: {{ printf "$%.2f" .Price }}
-Price with Tax: {{ .PriceWithTax | printf "$%.2f" }}
+Price with Tax: {{ calctax .Price | printf "$%.2f" }}
 `
 
 func main() {
@@ -29,6 +25,11 @@ func main() {
 		Name:  "Lemonade",
 		Price: 2.16,
 	}
-	t := template.Must(template.New("").Parse(templateString))
+	fm := template.FuncMap{}
+	fm["calctax"] = func(price float32) float32 {
+		return price * (1 + tax)
+	}
+
+	t := template.Must(template.New("").Funcs(fm).Parse(templateString))
 	t.Execute(os.Stdout, p)
 }
