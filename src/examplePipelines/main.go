153fa7deda5ef3b013e59e2b533583570d831d5c
Jason Down <jdown@igloosoftware.com>
2018-04-12 22:05:09 -0400

    Example of pipelines with basic string constant template.

---
+++ src/examplePipelines/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"html/template"
+	"os"
+)
+
+const tax = 6.75 / 100
+
+type Product struct {
+	Name  string
+	Price float32
+}
+
+func (p Product) PriceWithTax() float32 {
+	return p.Price * (1 + tax)
+}
+
+const templateString = `
+{{ "Item Information" }}
+`
+
+func main() {
+	p := Product{
+		Name:  "Lemonade",
+		Price: 2.16,
+	}
+	t := template.Must(template.New("").Parse(templateString))
+	t.Execute(os.Stdout, p)
+}
